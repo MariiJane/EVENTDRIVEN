7a9c973b58d1e32f4c55753dda870b2d536a741a
Jane <mariijane818@gmail.com>
2025-10-23 12:08:58 +0800

    Add files via upload

---
+++ RegistrationTextFile/RegistrationTextFile/RegistrationTextFile/FrmStudentRecord.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace RegistrationTextFile
+{
+    partial class FrmStudentRecord
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            btnRegister = new Button();
+            btnFind = new Button();
+            btnUpload = new Button();
+            openFileDialog1 = new OpenFileDialog();
+            listView1 = new ListBox();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.Location = new Point(12, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(105, 21);
+            label1.TabIndex = 0;
+            label1.Text = "View Record";
+            // 
+            // btnRegister
+            // 
+            btnRegister.FlatStyle = FlatStyle.System;
+            btnRegister.Location = new Point(409, 39);
+            btnRegister.Name = "btnRegister";
+            btnRegister.Size = new Size(75, 23);
+            btnRegister.TabIndex = 2;
+            btnRegister.Text = "Register";
+            btnRegister.UseVisualStyleBackColor = true;
+            btnRegister.Click += btnRegister_Click;
+            // 
+            // btnFind
+            // 
+            btnFind.FlatStyle = FlatStyle.System;
+            btnFind.Location = new Point(409, 68);
+            btnFind.Name = "btnFind";
+            btnFind.Size = new Size(75, 23);
+            btnFind.TabIndex = 3;
+            btnFind.Text = "Find";
+            btnFind.UseVisualStyleBackColor = true;
+            btnFind.Click += btnFind_Click;
+            // 
+            // btnUpload
+            // 
+            btnUpload.FlatStyle = FlatStyle.System;
+            btnUpload.Location = new Point(409, 97);
+            btnUpload.Name = "btnUpload";
+            btnUpload.Size = new Size(75, 23);
+            btnUpload.TabIndex = 4;
+            btnUpload.Text = "Upload";
+            btnUpload.UseVisualStyleBackColor = true;
+            btnUpload.Click += btnUpload_Click;
+            // 
+            // openFileDialog1
+            // 
+            openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // listView1
+            // 
+            listView1.FormattingEnabled = true;
+            listView1.ItemHeight = 15;
+            listView1.Location = new Point(12, 39);
+            listView1.Name = "listView1";
+            listView1.Size = new Size(382, 169);
+            listView1.TabIndex = 5;
+            // 
+            // FrmStudentRecord
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(496, 225);
+            Controls.Add(listView1);
+            Controls.Add(btnUpload);
+            Controls.Add(btnFind);
+            Controls.Add(btnRegister);
+            Controls.Add(label1);
+            Name = "FrmStudentRecord";
+            Text = "FrmStudentRecord";
+            Load += FrmStudentRecord_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Button btnRegister;
+        private Button btnFind;
+        private Button btnUpload;
+        private OpenFileDialog openFileDialog1;
+        private ListBox listView1;
+    }
+}
---
+++ RegistrationTextFile/RegistrationTextFile/RegistrationTextFile/FrmStudentRecord.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RegistrationTextFile
+{
+    public partial class FrmStudentRecord : Form
+    {
+        public FrmStudentRecord()
+        {
+            InitializeComponent();
+        }
+
+        private void FrmStudentRecord_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnRegister_Click(object sender, EventArgs e)
+        {
+            FrmRegistration registrationForm = new FrmRegistration();
+            registrationForm.Show();
+        }
+
+        private void btnFind_Click(object sender, EventArgs e)
+        {
+            DisplayToList();
+        }
+        public void DisplayToList()
+        {
+            openFileDialog1.InitialDirectory = @"C:\";
+            openFileDialog1.Title = "Browse Text Files";
+            openFileDialog1.DefaultExt = "txt";
+            openFileDialog1.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+            openFileDialog1.ShowDialog();
+
+            string path = "";
+            path = openFileDialog1.FileName;
+
+            using (StreamReader streamReader = File.OpenText(path))
+            {
+                string _getText = "";
+                while ((_getText = streamReader.ReadLine()) != null)
+                {
+                    Console.WriteLine(_getText);
+
+                    listView1.Items.Add(_getText);
+                }
+            }
+        }
+        private void btnUpload_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Successfully Uploaded");
+        }
+    }
+}
